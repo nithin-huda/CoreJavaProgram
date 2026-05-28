c5a36fa9c79dc141fa35ac8ac97e72a21c080147
Niteenkumar <pampannahs79@gmail.com>
2026-05-28 19:16:13 +0530

    First commit

---
+++ CoreJavaProgram/src/com/TNS/CoreJava/Demo.java
@@ -0,0 +1,20 @@
+package com.TNS.CoreJava;
+
+public class Demo {
+int n=116;
+String Name="Niteenkumar";
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		int a=10;
+		String Name="Niteenkumar";
+		System.out.println("hello java");
+		Demo d=new Demo();
+System.out.println(d.n);
+//System.out.println("hello java");
+System.out.println(a);
+System.out.println(d.Name);
+System.out.println(Name);
+	}
+
+}
